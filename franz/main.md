49588a0f95db98915a1c21eeb5e995dd6933bf06
Sven <sven@zemp.email>
2026-01-22 23:51:50 +0100

    Added Lambda and worked on workflow

---
+++ franz/main.md
@@ -0,0 +1,34 @@
+## Themen Uebersicht
+
+
+Algemeine Informationen
+- Karte (Wo, Hauptstatd, wie Gross, Vergleiche zur Schweiz, wie viele Menschen)
+
+
+Geschichte
+
+- Warum Senegal von den Franzosen kolonialkisiert
+- wann unabhaehnign\
+- politische situation heute
+- Kulturele Informationen ( sehenwuerdigskeiten)
+- stand der Franzoesichen sprache
+
+
+
+Wirtschaft 
+- BIP 
+- Primare Sektor (Vergleiche)
+- Sekundaer Sektor 
+- 3.Sektor 
+
+- Was exportiert es 
+- Handelspartner
+
+
+
+Ein aktuelles Problem
+1. 1 aktuelles Problem des Landes mit einer Loesung 
+
+
+
+2 Text :
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,16 +4,19 @@ let
 in
 {
   imports =
-    (if role == "web" then [ ./modules/web.nix ]
-     else if role == "db"  then [ ./roles/db.nix ]
-     else if role == "ci"  then [ ./roles/ci.nix ]
-     else [ ]);
+    (if role == "web" then [ ./modules/web.nix ./hardware-configuration ]
+     else if role == "db"  then [ ./roles/db.nix ./hardware-configuration ]
+     else if role == "ci"  then [ ./roles/ci.nix ./hardware-configuration ]
+     else [ ./hardware-configuration ]);
 
-  # shared stuff
   services.openssh.enable = true;
-  users.users.youruser = {
+  networking = {
+    hostName = "${role}";
+    nameservers = ["192.168.1.76" "1.1.1.1"];
+}
+  users.users.worker = {
     isNormalUser = true;
     initialPassword = "changeme";
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3…" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOEX55tH1gn4+Bsypr+2u5qwKmfOG68peFII4p1si7q/ thalium@cachyos-x8664" ];
   };
 }
